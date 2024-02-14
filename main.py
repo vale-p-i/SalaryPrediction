981cc35e67a4aab7690059638ebb9481dee24a96
vale-p-i <valeria.piscitelli@outlook.it>
2024-02-14 20:41:17 +0100

    Added distribution comparison and  final part (cross-validatio). Moreover, added more structure to the project.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
-from data_preparation import *
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from utils import *
+from sklearn.model_selection import KFold
 import pandas as pd
 import copy
-import random
 
+hr = "–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––"
+
+# cleaning output dir
+clear_dir('./output')
 # uploading dataset
 print("Uploading Dataset...")
 dataset_path = 'input/salary.csv'
@@ -15,8 +17,16 @@ df = pd.read_csv(dataset_path)
 print("Removing Missing Values...")
 df = remove_missing_values(df, ' ?')
 
+# salary binary encoding
+label_encoder = LabelEncoder()
+df['salary'] = label_encoder.fit_transform(df['salary'])
+
 # scaling
-print("Scaling...")
+print(hr)
+print("FEATURE SCALING")
+print(hr)
+
+# minmax normalization
 minmax_scaler = MinMaxScaler()
 df['age_scaled'] = minmax_scaler.fit_transform(df[['age']])
 df['hours-per-week_scaled'] = minmax_scaler.fit_transform(df[['hours-per-week']])
@@ -27,7 +37,12 @@ df['capital-loss_scaled'] = minmax_scaler.fit_transform(df[['capital-loss']])
 df['capital-gain_log'] = np.log(df['capital-gain'] + 1)
 df['capital-loss_log'] = np.log(df['capital-loss'] + 1)
 
+print("New feature created: age_scaled, hours-per-week_scaled, capital-gain_scaled, capital-loss_scaled")
+
 # categorize age, hours-per-week, capital-gain and capital-loss
+print(hr)
+print("CREATING CATEGORIZED FEATURES")
+print(hr)
 print("Categorizing...")
 for i, row in df.iterrows():
     age = row['age']
@@ -64,42 +79,149 @@ df = df.replace([' Doctorate', ' Prof-school'], ' Doc-Prof')
 df['new_marital-status'] = df['marital-status']
 df['new_marital-status'] = df['new_marital-status'].replace([' Married-civ-spouse', ' Married-AF-spouse'], ' Married')
 df['new_marital-status'] = df['new_marital-status'].replace(' Married-spouse-absent', ' Separated')
+distribution_comparison(df, 'age', 'age_cat', 'Age')
+distribution_comparison(df, 'hours-per-week', 'hours-per-week_cat', 'HoursPerWeek')
+distribution_comparison(df, 'marital-status', 'new_marital-status', 'Marital Status')
+distribution_comparison(df, 'education-num', 'education', 'Education')
+print("New feature created: age_cat, hours-per-week_cat, capital_cat, new_marital-status.")
+print("Changed education feature")
+print(hr)
 
 # removing irrelevant data
+print("REMOVING IRRELEVANT DATA")
+print(hr)
 df = df[df['workclass'] != ' Without-pay']
 df = df[df['occupation'] != ' Armed-Forces']
+print("Removed Without-pay and Armed-Forces categories")
 
 # encoding
+print(hr)
+print("ENCODING")
+print(hr)
 print("Encoding...")
 mapping = {' Before-HS': 0, ' HS-grad': 1, ' Some-college': 2, ' Assoc': 3, ' Bachelors': 4, ' Masters': 5,
            ' Doc-Prof': 6}
 df['education'] = df['education'].map(mapping)
-label_encoder = LabelEncoder()
-df['salary'] = label_encoder.fit_transform(df['salary'])
 
+# feature selection
+print(hr)
+print("FEATURE SELECTION")
+print(hr)
 df = df.reindex(sorted(df.columns), axis=1)
-df.info()
 num_features = [i for i in df if df[i].dtype != 'object' and df[i].dtype != 'category']
-print(num_features)
-
+print("Generation Correlation Matrix")
 generete_correlation_matrix(df[num_features])
-
+print("Removing unselected features")
 df = df.drop(columns=['native-country', 'relationship', 'fnlwgt', 'education-num', 'marital-status'])
 
+# one hot encoding
+print(hr)
+print("ONE HOT ENCODING")
+print(hr)
 cat_features = [i for i in df if df[i].dtype == 'object' or df[i].dtype == 'category']
-print(cat_features)
 df_encoded = pd.get_dummies(df, columns=cat_features, dtype=int)
+
+# Cross validation
+print(hr)
+print("CROSS VALIDATION")
+print(hr)
 
 y = df['salary']
 X = df_encoded.drop(columns=['salary'])
 
+kf = KFold(n_splits=10)
+
 col_to_test = ['age_scaled', 'age_cat', 'age', 'hours-per-week', 'hours-per-week_cat', 'hours-per-week_scaled',
                'capital-loss_log', 'capital-gain_log', 'capital-gain', 'capital-loss', 'capital-gain_scaled',
                'capital-loss_scaled', 'capital_cat']
+
+# age cat vs. age scaled
+# age cat
 col = copy.deepcopy(col_to_test)
-for i in ['age_cat', 'hours-per-week_cat', 'capital_cat']:
+features = ['age_cat', 'hours-per-week', 'capital-gain', 'capital-loss']
+for i in features:
     col.remove(i)
 X1 = X.drop(columns=col)
-x = random.randint(0, 256)
-X_train, X_test, y_train, y_test = train_test_split(X1, y, test_size=0.33, random_state=x)
-fit_and_test(DecisionTreeClassifier(), X_train, y_train, X_test, y_test)
+cross_validation(X1, y, features, kf, "age categorized", "cat-vs-scaledRESULTS.txt")
+# age scaled
+col = copy.deepcopy(col_to_test)
+features = ['age_scaled', 'hours-per-week', 'capital-gain', 'capital-loss']
+for i in features:
+    col.remove(i)
+X1 = X.drop(columns=col)
+cross_validation(X1, y, features, kf, "age scaled", "cat-vs-scaledRESULTS.txt")
+
+# hpw cat vs. hpw scaled
+# hpw cat
+col = copy.deepcopy(col_to_test)
+features = ['age_cat', 'hours-per-week_cat', 'capital-gain', 'capital-loss']
+for i in features:
+    col.remove(i)
+X1 = X.drop(columns=col)
+cross_validation(X1, y, features, kf, "hours per week categorized", "cat-vs-scaledRESULTS.txt")
+
+# hpw scaled
+col = copy.deepcopy(col_to_test)
+features = ['age_cat', 'hours-per-week_scaled', 'capital-gain', 'capital-loss']
+for i in features:
+    col.remove(i)
+X1 = X.drop(columns=col)
+cross_validation(X1, y, features, kf, "hours per week scaled", "cat-vs-scaledRESULTS.txt")
+
+# capital gain & capital loss cat vs. scaled
+# cat
+col = copy.deepcopy(col_to_test)
+features = ['age_cat', 'hours-per-week_cat', 'capital_cat']
+for i in features:
+    col.remove(i)
+X1 = X.drop(columns=col)
+cross_validation(X1, y, features, kf, "capital categorized", "cat-vs-scaledRESULTS.txt")
+
+# minmax scaled
+col = copy.deepcopy(col_to_test)
+features = ['age_cat', 'hours-per-week_cat', 'capital-gain_scaled', 'capital-loss_scaled']
+for i in features:
+    col.remove(i)
+X1 = X.drop(columns=col)
+cross_validation(X1, y, features, kf, "Capital gain and capital log scaled", "cat-vs-scaledRESULTS.txt")
+
+# log trasformation
+col = copy.deepcopy(col_to_test)
+features = ['age_cat', 'hours-per-week_cat', 'capital-gain_log', 'capital-loss_log']
+for i in features:
+    col.remove(i)
+X1 = X.drop(columns=col)
+cross_validation(X1, y, features, kf, "Capital gain log and capital loss log", "cat-vs-scaledRESULTS.txt")
+
+# finding discriminating data
+print(hr)
+print("FINDING DISCRIMINATING DATA")
+print(hr)
+col_to_remove = ['age_scaled', 'age', 'hours-per-week', 'hours-per-week_scaled', 'capital-gain', 'capital-loss',
+                 'capital-gain_scaled', 'capital-loss_scaled', 'capital_cat']
+X = X.drop(columns=col_to_remove)
+
+# testing removal of sex feature
+X1 = X.drop(columns=['sex_ Female', 'sex_ Male'])
+cross_validation(X1, y, X1.columns, kf, "sex", "testing-of-discriminating-featuresRESULTS.txt")
+
+# testing removal of race feature
+X1 = X.drop(columns=['race_ Asian-Pac-Islander', 'race_ Amer-Indian-Eskimo', 'race_ Black', 'race_ Other', 'race_ White'])
+cross_validation(X1, y, X1.columns, kf, "race", "testing-of-discriminating-featuresRESULTS.txt")
+
+# testing removal of age_cat feature
+X1 = X.drop(columns=['age_cat'])
+cross_validation(X1, y, X1.columns, kf, "age categorized", "testing-of-discriminating-featuresRESULTS.txt")
+
+# testing removal of new_marital-status
+X1 = X.drop(columns=['new_marital-status_ Divorced', 'new_marital-status_ Married', 'new_marital-status_ Never-married', 'new_marital-status_ Separated', 'new_marital-status_ Widowed'])
+cross_validation(X1, y, X1.columns, kf, "marital status", "testing-of-discriminating-featuresRESULTS.txt")
+
+# testing removal of sex and race
+X1 = X.drop(columns=['sex_ Female', 'sex_ Male', 'race_ Asian-Pac-Islander', 'race_ Amer-Indian-Eskimo', 'race_ Black', 'race_ Other', 'race_ White'])
+cross_validation(X1, y, X1.columns, kf, "sex and race", "testing-of-discriminating-featuresRESULTS.txt")
+
+# testing removal of sex, race and age
+X1 = X.drop(columns=['age_cat', 'sex_ Female', 'sex_ Male', 'race_ Asian-Pac-Islander', 'race_ Amer-Indian-Eskimo', 'race_ Black', 'race_ Other', 'race_ White'])
+cross_validation(X1, y, X1.columns, kf, "sex, race and age", "testing-of-discriminating-featuresRESULTS.txt")
+
--- utils.py
+++ utils.py
@@ -5,9 +5,10 @@ import seaborn as sns
 import os
 import shutil
 import statistics
-from sklearn.metrics import accuracy_score, recall_score, confusion_matrix, ConfusionMatrixDisplay, precision_score
 from sklearn.model_selection import cross_validate
 from sklearn.tree import DecisionTreeClassifier
+
+hr = '----------------------------------------------------------------------------------------------------'
 
 
 def remove_missing_values(df: pd.DataFrame, missing_value: str) -> pd.DataFrame:
@@ -23,40 +24,21 @@ def generete_correlation_matrix(columns):
     plt.savefig('output/correlationMatrix.png')
 
 
-def generate_confusion_matrix(y_test, predictions):
-    cm = confusion_matrix(y_test, predictions, labels=[0, 1])
-    display = ConfusionMatrixDisplay(confusion_matrix=cm)
-    display.plot()
-    plt.savefig('output/confusion_matrix.png')
-
-
-def fit_and_test(classifier, X_train, y_train, X_test, y_test):
-    print(classifier.__class__.__name__)
-    classifier.fit(X_train, y_train)
-    predictions = classifier.predict(X_test)
-
-    # confusion matrix
-    generate_confusion_matrix(y_test, predictions)
-
-    # show metrics
-    accurancy = accuracy_score(y_test, predictions)
-    print("Accurancy:" + str(accurancy))
-    recall = recall_score(y_test, predictions)
-    print("Recall:" + str(recall))
-    precision = precision_score(y_test, predictions)
-    print("Precision:" + str(precision))
-
-
-def cross_validation(X_train, y_train, features, validator):
+def cross_validation(X, y, features, validator, feature_tested, filename):
+    report = open('output/'+filename, 'a')
     tests = list(["accuracy", "precision", "recall"])
-    cv_score = cross_validate(DecisionTreeClassifier(), X_train, y_train, cv=validator, n_jobs=3, verbose=0, scoring=tests)
+    cv_score = cross_validate(DecisionTreeClassifier(), X, y, cv=validator, n_jobs=3, verbose=0, scoring=tests)
     accuracy_mean = statistics.mean(cv_score['test_accuracy'])
     precision_mean = statistics.mean(cv_score['test_precision'])
     recall_mean = statistics.mean(cv_score['test_recall'])
-    print("Using features: " + str(features))
-    print("Accuracy: " + str(accuracy_mean))
-    print("Precision: " + str(precision_mean))
-    print("Recall: " + str(recall_mean))
+    report.write("\n")
+    report.write("TESTING " + feature_tested + "\n")
+    report.write("Using features: " + str(features) + "\n")
+    report.write("Precision: " + str(precision_mean) + "\n")
+    report.write("Recall: " + str(recall_mean) + "\n")
+    report.write("Accuracy: " + str(accuracy_mean) + "\n")
+    report.write("\n")
+    report.write(hr + "\n")
 
 
 def clear_dir(directory):
@@ -76,3 +58,13 @@ def clear_dir(directory):
             print(f"La cartella {directory} non esiste.")
     except Exception as e:
         print(f"Errore generale durante la pulizia della cartella {directory}: {e}")
+
+
+def distribution_comparison(df: pd.DataFrame, colname_1, colname_2, varname):
+    fig, ax = plt.subplots(2, 1, figsize=(20, 16))
+    sns.countplot(x=colname_1, hue='salary', data=df, palette='Set2', ax=ax[0])
+    ax[0].set_title(f'Before ( {varname} Distribution)')
+    sns.countplot(x=colname_2, hue='salary', data=df, palette='Set2', ax=ax[1])
+    ax[1].set_title(f'After ( {varname} Distribution)')
+    plt.savefig(f'output/{varname}DistributionComparison.png')
+    plt.close()
